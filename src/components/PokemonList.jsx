617a8125420b9c2ca3301631c1ed1fc90530118a
Aliev Madjit <madzitaliev51@gmail.com>
2024-10-26 18:26:30 +0600

    Добавил от себя пагинацию (страницы)  и добавил стили

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,18 +3,21 @@ import PokemonCard from "./PokemonCard";
 
 const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
+  const [page, setPage] = useState(1);
+  const limit = 20;
 
   useEffect(() => {
     const fetchPokemonList = async () => {
+      const offset = (page - 1) * limit;
       const response = await fetch(
-        `https://pokeapi.co/api/v2/pokemon`
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
       );
       const data = await response.json();
       setPokemonList(data.results);
     };
 
     fetchPokemonList();
-  }, []);
+  }, [page]);
 
   return (
     <div>
@@ -27,7 +30,22 @@ const PokemonList = () => {
           />
         ))}
       </div>
- 
+      <div className="pagination">
+        <button
+          className="last-button"
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+        >
+          Назад
+        </button>
+        <span>Страница {page}</span>
+        <button
+          className="next-button"
+          onClick={() => setPage(page + 1)}
+        >
+          Вперед
+        </button>
+      </div>
     </div>
   );
 };
